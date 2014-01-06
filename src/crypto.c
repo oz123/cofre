0a6483f564eff8c484fd5cd5b35e3dd0620f9768
oz123 <nahumoz@gmail.com>
2014-01-06 11:42:09 +0100

    add crypto.c and driver.c

---
+++ src/crypto.c
@@ -0,0 +1,16 @@
+#include  "crypto.h"
+
+
+
+/* 
+ * Check if user input for password is corred
+ * if so, the return the enc_key as decryption 
+ * chyper and store True in opened. 
+ * */
+void crypto_get_key(char * enc_key, bool * opened){
+
+    if (true) {
+        *opened =  true;
+    }
+
+}
---
+++ src/driver.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "driver.h"
+
+/*
+ * Open the db file_name 
+ * return 0 if OK
+ * return 1 if not 
+ */
+int drvr_opendb(const char *file_name ){
+
+   sqlite3 *db;
+   //char *zErrMsg = 0;
+   int rc;
+
+   rc = sqlite3_open("test.db", &db);
+
+   if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+   }else{
+      fprintf(stderr, "Opened database successfully\n");
+   }
+   return 0;
+}
