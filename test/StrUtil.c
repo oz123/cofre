c76b0e4ab823db487b5e7e183a7a99c6cc169eb4
oz123 <nahumoz@gmail.com>
2014-03-15 10:26:05 +0100

    Remove strdup, it's not c99 compatible

--- test/StrUtil.c
+++ test/StrUtil.c
@@ -9,7 +9,7 @@
     }
     
     void TestStrToUpper(CuTest *tc) {
-        char* input = strdup("hello world");
+        char* input = "hello world";
         char* actual = StrToUpper(input);
         char* expected = "HELLO WORLD";
         CuAssertStrEquals(tc, expected, actual);
--- test/TestCli.c
+++ test/TestCli.c
@@ -1,20 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "CuTest.h"
-
+#include "../src/driver.h"
 
     char* BtrToUpper(char* str) {
         return str;
     }
     
     void TestBtrToUpper(CuTest *tc) {
-        char* input = strdup("hello world");
+        char *input = "hello world";
         char* actual = BtrToUpper(input);
         char* expected = "HELLO WORLD";
         CuAssertStrEquals(tc, expected, actual);
     }
-   
+
     CuSuite* BtrUtilGetSuite() {
         CuSuite* suite = CuSuiteNew();
         SUITE_ADD_TEST(suite, TestBtrToUpper);
