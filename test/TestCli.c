165492e3cac0af15376bde098b6733915d0ba05c
oz123 <nahumoz@gmail.com>
2014-03-22 00:25:47 +0100

    Added popen wrapper for the testing

--- test/TestCli.c
+++ test/TestCli.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE  2
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,8 +9,35 @@
 #include "../src/driver.h"
 
 sqlite3 *pconn ; // malloc(sizeof(sqlite3*));
+#define DATA_SIZE 512
+ 
+void get_sqlite_data(char *out){
+    FILE *pf;
+    
+    // Execute a process listing
+    char *command = "sqlite3 -line test_db.db '.schema'";
+    
+    // Setup our pipe for reading and execute our command.
+    pf = popen(command,"r"); 
+ 
+    if(!pf){
+      fprintf(stderr, "Could not open pipe for output.\n");
+      return;
+    }
+ 
+    // Grab data from process execution
+    fgets(out, DATA_SIZE , pf);
+    
+    // Print grabbed data to the screen.
+    //fprintf(stdout, "-%s-\n",out); 
+ 
+    if (pclose(pf) != 0)
+        fprintf(stderr," Error: Failed to close command stream \n");
+ 
+    return;
+}
 
-    char* StrToUpper(char* str) {
+char* StrToUpper(char* str) {
         int i = 0;
         while ( str[i] != '\0'){
             str[i] = toupper(str[i]);
@@ -18,7 +46,7 @@ sqlite3 *pconn ; // malloc(sizeof(sqlite3*));
         return str;
     }
     
-    void TestStrToUpper(CuTest *tc) {
+void TestStrToUpper(CuTest *tc) {
         // This is implicit const char *input!
         // This is why it will cause segmentation fault
         // We can't change a const variable
@@ -29,23 +57,21 @@ sqlite3 *pconn ; // malloc(sizeof(sqlite3*));
         CuAssertStrEquals(tc, expected, actual);
     }
 
-    void TestDriver(CuTest *tc) {
-        drvr_opendb("test_db.db", &pconn);
-        drvr_check_tables(&pconn);
-        CuAssertTrue(tc, ! access("test_db.db", F_OK ));
+void 
+TestDriver(CuTest *tc) {
+     drvr_opendb("test_db.db", &pconn);
+     drvr_check_tables(&pconn);
+     CuAssertTrue(tc, ! access("test_db.db", F_OK ));
+     char output[512];
+     get_sqlite_data(output);
+     printf("this is the output: %s", output);
+     char * expected = "CREATE TABLE COMPANY(ID INT PRIMARY KEY     "\
+                       "NOT NULL,NAME           TEXT    NOT NULL,AGE            INT     "\
+                       "NOT NULL,ADDRESS        CHAR(50),SALARY         REAL );";
         
-        char * expected = "CREATE TABLE COMPANY(ID INT PRIMARY KEY     "\
-                          "NOT NULL,NAME           TEXT    NOT NULL,AGE            INT     "\
-                          "NOT NULL,ADDRESS        CHAR(50),SALARY         REAL );";
-        
-        char * command = "sqlite3 -line test/test_db.db '.schema'";
-    }
 
-    /*void TestDriverHasTable(CuTest *tc) {
-        drvr_check_tables("test_db.db");
-        CuAssertTrue(tc, ! access("test_db.db", F_OK ));
-    }*/
-   
+}
+
     CuSuite* StrUtilGetSuite() {
         CuSuite* suite = CuSuiteNew();
         SUITE_ADD_TEST(suite, TestDriver);
